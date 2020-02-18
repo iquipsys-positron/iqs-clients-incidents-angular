99d161ec939fec880d3ab8040aadf215b848821b
Aleksandr Koshevierov <iworb.ne@gmail.com>
2020-02-18 09:55:56 +0300

    Configurable

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,7 @@
 (() => {
     function iqsPositronIncidentsConfig(
-        pipActionsProvider: pip.nav.IActionsProvider,
-        pipAuthStateProvider: pip.rest.IAuthStateProvider,
-        pipErrorPageConfigServiceProvider: pip.errors.IErrorPageConfigProvider,
+        pipActionsProvider: pip.nav.IActionsProvider
     ) {
-        pipAuthStateProvider.authorizedState = 'app.statistics';
-        pipErrorPageConfigServiceProvider.configs.NoConnection.RedirectSateDefault = pipAuthStateProvider.authorizedState;
-
         pipActionsProvider.primaryGlobalActions.unshift(...[
             { name: 'global.incidents', icon: 'icons:bell', count: 0, event: 'iqsIncidentsOpen' },
         ]);
